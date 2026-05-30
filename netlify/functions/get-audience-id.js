561545e29bd7a3112a933b3d12c143027d0d6fb9
David Alejandro Soler <dasoler@gmail.com>
2026-05-29 23:30:19 -0500

    fix: Resend key via env var

--- netlify/functions/get-audience-id.js
+++ netlify/functions/get-audience-id.js
@@ -1,6 +1,6 @@
 // Función temporal para obtener el Audience ID
 exports.handler = async function(event, context) {
-  const RESEND_KEY = process.env.RESEND_API_KEY_FULL || 're_j34jvzpu_KpL4VdQqX312qqzUiaiwLsEC';
+  const RESEND_KEY = process.env.RESEND_API_KEY_FULL || process.env.RESEND_API_KEY_FULL || '';
   
   const response = await fetch('https://api.resend.com/audiences', {
     headers: { 'Authorization': `Bearer ${RESEND_KEY}` }
